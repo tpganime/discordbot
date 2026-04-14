8e573b0420565d466119dda68c0ed67d7a57ee23
tpganime <139879328+tpganime@users.noreply.github.com>
2026-04-14 08:35:07 +0530

    fix: Update support server invite URL
    The previous invite URL for the FUSION｜BOT support server was invalid. This commit updates it to the correct, working URL.

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const APP_NAME = 'FUSION BOT';
 export const SUPPORT_EMAIL = 'fusionhub122@gmail.com';
 export const DISCORD_INVITE_URL = 'https://discord.com/oauth2/authorize?client_id=1485375910562758967&permissions=8&integration_type=0&scope=bot';
 export const DASHBOARD_URL = 'https://panel.fusionhub.in/';
-export const SUPPORT_SERVER_URL = 'https://discord.gg/m8By8h9PPy';
+export const SUPPORT_SERVER_URL = 'https://discord.gg/qc26U4WVfF';
 export const COMMUNITY_URL = 'https://discord.gg/QK8KJrZv55';
 export const YOUTUBE_URL = 'https://www.youtube.com/@tpggamer1';
 export const LOGO_URL = 'https://cdn.discordapp.com/attachments/1479524006242549979/1485250262103887982/IMG_20260322_174353.jpg?ex=69da3b19&is=69d8e999&hm=d1c9fec37f35d6374706da08174f98a95ef507ef5b67b26860aea5d844864913';
